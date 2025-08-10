16dfdbd17473358505d17a35c028918716d1c62b
Moussa Hassana <moussahassana42@gmail.com>
2025-08-10 10:44:03 +0100

    feat: add CRUD viewsets for product_category and product with audit fields

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import ProductCategory, Product
+from .serializers import ProductCategorySerializer, ProductSerializer
+from .filters import ProductFilter
 
-# Create your views here.
+class ProductCategoryViewSet(viewsets.ModelViewSet):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    lookup_field = "slug"
+    filterset_fields = ["is_active"]
+    search_fields = ["name"]
+    ordering_fields = ["name"]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user, updated_by=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(updated_by=self.request.user)
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.select_related("product_category").prefetch_related("images")
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    lookup_field = "slug"
+    filterset_class = ProductFilter
+    search_fields = ["title", "sku", "description"]
+    ordering_fields = ["price", "created_at", "title"]
+    ordering = ["-created_at"]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user, updated_by=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(updated_by=self.request.user)
