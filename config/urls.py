4210e35f5aae753a28bb4de8db05a8085a191cb7
Moussa Hassana <moussahassana42@gmail.com>
2025-08-10 12:21:56 +0100

    feat: redirect root URL to Swagger docs

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -10,4 +11,5 @@ urlpatterns = [
     path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("catalog.urls")),
+     path('', lambda request: redirect('/api/docs', permanent=False)),
 ]
